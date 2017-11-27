e7ef7cc52a34a775fae564dbcdcef5120795911f
ajinkyaghorpade <ajinkyaghorpade@gmail.com>
2017-11-27 15:40:02 -0500

    Change the version of the sofwares to match the engaging software

--- mxnet/install_cuda.sh
+++ mxnet/install_cuda.sh
@@ -1,4 +1,4 @@
-driver_version=367.44
+driver_version=375.20
 
 sh /mnt/NVIDIA-Linux-x86_64-$driver_version.run -x
 mv NVIDIA-Linux-x86_64-$driver_version /usr/local/
@@ -6,10 +6,10 @@ sh /mnt/links.sh $driver_version
 
 apt-get -y --force-yes install perl gcc
 
-sh /mnt/cuda_8.0.44_linux-run --toolkit --silent --toolkitpath=/usr/local/cuda-8.0
+sh /mnt/cuda_8.0.61_linux-run --toolkit --silent --toolkitpath=/usr/local/cuda-8.0
 cp -r -f --symbolic-link /usr/local/cuda-8.0/* /usr/local/cuda/
 
-tar xvf /mnt/cudnn-8.0-linux-x64-v5.1.tgz -C /usr/local
+tar xvf /mnt/cudnn-8.0-linux-x64-v7.tgz -C /usr/local
 
 driver_path=/usr/local/NVIDIA-Linux-x86_64-$driver_version
 echo "LD_LIBRARY_PATH=/usr/local/cuda/lib64:$driver_path:$LD_LIBRARY_PATH" >> /environment
